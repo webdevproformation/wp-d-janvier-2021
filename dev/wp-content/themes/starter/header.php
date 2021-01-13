90d2755c49026990cd312b22ea096d9cdb52847c
malik <malik.h@webdevpro.net>
2021-01-13 11:57:29 +0100

    barre de menu

--- dev/wp-content/themes/starter/header.php
+++ dev/wp-content/themes/starter/header.php
@@ -9,11 +9,30 @@
 </head>
 <body>
     <div class="container">
-        <header class="row">
+        <header class="row mb-3">
             <div class="col-4">
-                <h1>Mon site</h1>
+                <h1 class="h2"><?php echo get_option("blogname") ?></h1>
+                <p class="mb-0"><?php echo get_option("blogdescription") ?></p>
             </div>
-            <nav class="col-8">
-                <h1>barre de menu du site</h1>
+            <nav class="col-8 navbar navbar-expand navbar-light bg-white">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Présentation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Nous contacter</a>
+                    </li>
+                </ul>
+                <form class="form-inline d-flex">
+                    <input type="text" class="form-control">
+                    <input type="submit" class="btn btn-outline-dark btn-sm ms-2">
+                </form>
             </nav>
+           
         </header>
