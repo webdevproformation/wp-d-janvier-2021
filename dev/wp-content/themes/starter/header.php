054f169e75b28d960fc530d9a4a34f7d7deb9c58
malik <malik.h@webdevpro.net>
2021-01-13 11:32:46 +0100

    commencer le thème

---
+++ dev/wp-content/themes/starter/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil de mon site</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <?php wp_head() ?>
+</head>
+<body>
+    <div class="container">
+        <header class="row">
+            <div class="col-4">
+                <h1>Mon site</h1>
+            </div>
+            <nav class="col-8">
+                <h1>barre de menu du site</h1>
+            </nav>
+        </header>
---
+++ dev/wp-content/themes/starter/index.php
@@ -0,0 +1,5 @@
+<?php get_header() ?>
+    <section class="row">
+        <h2>Contenu de la page </h2>
+    </section>
+<?php get_footer() ?>
