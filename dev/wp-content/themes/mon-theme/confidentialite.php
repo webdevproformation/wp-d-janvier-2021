f34a913b5e2b796ba87361216945b8c136d6a5c7
malik <malik.h@webdevpro.net>
2021-01-14 10:38:41 +0100

    modele de page

---
+++ dev/wp-content/themes/mon-theme/confidentialite.php
@@ -0,0 +1,20 @@
+<?php
+    /*
+    Template Name: politique de confidentialité
+    */
+?>
+<!-- modèle de page -->
+<?php get_header() ?>
+    <?php if( have_posts()) : ?>
+        <?php while( have_posts() ) : ?>
+            <?php the_post() ?>
+            <div class="col-3">
+                <?php the_title( "<h1>", "</h1>" ) ?>
+                <?php edit_post_link( "modifier", "<div>", "</div>", $post, "btn btn-outline-success") ?>
+            </div>
+            <div class="col-9">
+                <?php the_content() ?>
+            </div>
+        <?php endwhile ?>
+    <?php endif  ?>
+<?php get_footer() ?>
