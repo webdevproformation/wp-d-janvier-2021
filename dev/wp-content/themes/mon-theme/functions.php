81db2019c4437da85529c329044bf0c6a0806675
malik <malik.h@webdevpro.net>
2021-01-14 12:35:18 +0100

    style css dans wordpress fin

--- dev/wp-content/themes/mon-theme/functions.php
+++ dev/wp-content/themes/mon-theme/functions.php
@@ -9,3 +9,20 @@ function mon_theme_image_a_la_une(){
 // exécuter la fonction précédente lorsque le thème est activé 
 
 add_action( "after_setup_theme", "mon_theme_image_a_la_une" );
+
+// ajouter les css de mon thème 
+
+function mon_theme_css(){
+    // mon thème j'ai ajouté 4 feuilles de style :
+    // 1 bootstrap 
+    wp_enqueue_style( "bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css", [], "5.0.0-beta1", "all" );
+    // 2 bootstrap icon
+    wp_enqueue_style( "bootstrap-icon", "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css", ["bootstrap"], "1.3.0", "all" );
+    // 3 mon-theme.css 
+    wp_enqueue_style( "mon-theme", get_template_directory_uri() ."/css/mon-style.css" , [], "1.0",  "all" );
+    // 4 style.css 
+    wp_enqueue_style( "style", get_template_directory_uri() ."/style.css" , [], "1.0",  "all" );
+}
+
+
+add_action("wp_enqueue_scripts" , "mon_theme_css" );
--- dev/wp-content/themes/mon-theme/header.php
+++ dev/wp-content/themes/mon-theme/header.php
@@ -4,10 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ."/css/mon-style.css"?>">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . "/style.css" ?>">
     <?php wp_head() ?>
 </head>
 <body>
