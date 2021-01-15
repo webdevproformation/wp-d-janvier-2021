24f8368023f609d5747e90553d2945cd29962761
malik <malik.h@webdevpro.net>
2021-01-15 10:03:00 +0100

    modifié le formulaire appeler le template search

--- dev/wp-content/themes/mon-theme/header.php
+++ dev/wp-content/themes/mon-theme/header.php
@@ -11,8 +11,8 @@
         <header class="row">
             <nav class="col-12 navbar navbar-expand bg-dark navbar-dark">
                 <?php echo mon_theme_generate_menu() ?>
-                <form action="#" class="d-flex ms-2">
-                    <input type="text" name="s" placeholder="rechercher" class="form-control py-1 px-3">
+                <form action="<?php echo esc_url(home_url( '/' )); ?>" class="d-flex ms-2">
+                    <input type="text" name="s" placeholder="rechercher" class="form-control py-1 px-3" value="<?php echo get_search_query() ?>">
                     <input type="submit" class="btn btn-outline-light btn-sm ms-2 p-1">
                 </form>
             </nav>
