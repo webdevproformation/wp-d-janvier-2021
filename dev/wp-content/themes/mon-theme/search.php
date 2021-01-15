95e7266a056d888573967c3b2af63f4839971dc4
malik <malik.h@webdevpro.net>
2021-01-15 10:18:39 +0100

    page search

---
+++ dev/wp-content/themes/mon-theme/search.php
@@ -0,0 +1,24 @@
+<?php get_header() ?>
+
+<?php if( have_posts() ) : ?>
+    <!--  si on a des articles qui contiennent le mot clé recherché -->
+    <h2>voici les résultats pour <b><?php echo get_search_query() ?></b></h2>
+    <?php while( have_posts() ) : ?>
+        <?php the_post() ?>
+        <div class="col-6 mb-3">
+            <article class="card">
+                <header class="card-header">
+                    <?php the_title() ?>
+                </header>
+                <div class="card-header">
+                    <a href="<?php the_permalink() ?>" class="btn btn-outline-success">voir l'article</a>
+                </div>
+            </article>
+        </div>
+    <?php endwhile ?>
+<?php else : ?>
+    <h2>Aucun article trouvé pour le mot recherché</h2>
+    <!--  on n'a pas de contenu pour le texte recherché -->
+<?php endif ?>
+
+<?php get_footer() ?>
